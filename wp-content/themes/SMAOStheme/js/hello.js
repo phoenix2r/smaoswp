349ea9c0027c6a5c140bb6fbff6ea01b2c8a0d08
Richard Ross <richard.ross@sci-telligent.co.uk>
2020-07-14 09:30:41 +0100

    console log test

--- wp-content/themes/SMAOStheme/js/hello.js
+++ wp-content/themes/SMAOStheme/js/hello.js
@@ -1,3 +1,3 @@
-console.log("Hello World!!!");
+console.log("Hello World!!! - This is version 2");
 let pageTitles = document.querySelectorAll('.title');
 console.log("No. of titles: " + pageTitles.length);
